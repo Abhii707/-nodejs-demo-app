98301c4562904734bb1ae0cee4cfd22b89b3d0a6
abhishek <parjapati3642@gmail.com>
2025-08-04 19:07:57 +0530

    Initial commit with working Node.js app

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+const requestHandler = (req, res) => {
+  if (req.url === '/health') {
+    return res.end('OK');
+  }
+  res.end('Hello from Node.js app');
+};
+
+const server = http.createServer(requestHandler);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
